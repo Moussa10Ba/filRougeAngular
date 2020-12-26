f8e5dc4484126a8517a069e0a7fd222ac88a2772
moussa <moussa10ba@gmail.com>
2020-12-26 00:44:03 +0000

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {AuthComponent} from './auth/auth.component';
+import {UsersListComponent} from './users/users-list/users-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+ {path : '', redirectTo: '/login', pathMatch: 'full'},
+  {path: 'login', component: AuthComponent},
+  {path: 'listUsers', component: UsersListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { AuthComponent } from './auth/auth.component';
+import { UsersListComponent } from './users/users-list/users-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AuthComponent,
+    UsersListComponent
   ],
   imports: [
     BrowserModule,
